929b38044283cf657938715467ebe5fe0f5d5259
Blake Pfaff <blake.a.pfaff2@gmail.com>
2018-01-09 11:12:44 -0600

    adds completed boolean to items

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180105012514) do
+ActiveRecord::Schema.define(version: 20180109164151) do
 
   create_table "items", force: :cascade do |t|
     t.string "item"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "list_id"
+    t.boolean "compeleted"
     t.index ["list_id"], name: "index_items_on_list_id"
   end
 
